74d3bbbead0d07ea7d2fb55ef3eca46140ecd549
Nazmul Islam <nir71.ni@gmail.com>
2024-06-08 13:02:55 +0600

    Initial commit

---
+++ lib/utils/colors.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+const appPrimaryColor = Color(0xFF4293C1);
+const whiteColor = Colors.white;
+const blackColor = Colors.black;
+const redColor = Colors.red;
+const greyColor = Color(0xFFEEEEEE);
+const blueColor = Colors.blue;
+const List<Color> contactColorList = [
+  blueColor,
+  redColor,
+  appPrimaryColor,
+  Colors.purple,
+  Colors.pink,
+  Colors.lightBlue,
+  Colors.orange,
+  Colors.brown,
+  Colors.redAccent,
+];
